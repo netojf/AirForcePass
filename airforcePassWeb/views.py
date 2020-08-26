c24ff625359fe4462b9931dd97887ebc59491833
José Fernandes Rezende Neto <j.fr.neto@hotmail.com>
2020-08-26 12:24:58 -0300

    Initial commit -added sigin page and control

---
+++ airforcePassWeb/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+def userSigIn(request):
+    if request.method == "POST":
+        form_usuario = UserCreationForm(request.POST)
+        if form_usuario.is_valid():
+            form_usuario.save()
+            return redirect('index')
+    else:
+        form_usuario = UserCreationForm()
+    return render(request, 'cadastro.html', {'form_usuario': form_usuario})
